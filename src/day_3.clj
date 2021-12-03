8912637aabc164bb12cc0ecce67e7fcc66d26d43
Akaash Patnaik <akaashanky@gmail.com>
2021-12-03 18:17:39 +0530

    Day 3 refactored

--- src/day_3.clj
+++ src/day_3.clj
@@ -15,45 +15,41 @@
            0 input-ratings)
          (/ (count input-ratings) 2))
     \1 \0))
+(defn flip-bit [bit-char] (if (= bit-char \1) \0 \1))
 
-(defn get-epsilon-bit-from-gamma-bit [bit-char]
-  (if (= bit-char \1) \0 \1))
+(def get-epsilon-bit #(flip-bit (get-gamma-bit %1 %2)))
 
 (defn get-gamma-rate [input-ratings]
   (str/join (map #(get-gamma-bit input-ratings %1) (range 0 (count (first input-ratings))))))
 
 (defn get-epsilon-rate [input-ratings]
-  (str/join (map #(get-epsilon-bit-from-gamma-bit (get-gamma-bit input-ratings %1)) (range 0 (count (first input-ratings))))))
+  (str/join (map #(get-epsilon-bit input-ratings %1) (range 0 (count (first input-ratings))))))
 
-(defn get-ratings [input-str]
+(defn get-power-consumption-ratings [input-str]
   (let [input-ratings (parse-input input-str)]
     [(Integer/parseInt (get-gamma-rate input-ratings) 2)
      (Integer/parseInt (get-epsilon-rate input-ratings) 2)]))
 
 (defn part-1 [input-str]
-  (reduce * (get-ratings input-str)))
+  (reduce * (get-power-consumption-ratings input-str)))
 
 (defn matches-o2-criteria? [input-ratings position input]
   (= (get-gamma-bit input-ratings position) (.charAt input position)))
 
 (defn matches-co2-criteria? [input-ratings position input]
-  (= (get-epsilon-bit-from-gamma-bit (get-gamma-bit input-ratings position)) (.charAt input position)))
+  (= (get-epsilon-bit input-ratings position) (.charAt input position)))
 
-(defn get-o2-rating [input]
+(defn get-rating [match-criteria input]
   (loop [input-ratings input
          position 0]
     (if (= 1 (count input-ratings))
       (first input-ratings)
-      (recur (filter #(matches-o2-criteria? input-ratings position %1) input-ratings) (inc position)))
+      (recur (filter #(match-criteria input-ratings position %1) input-ratings) (inc position)))
     ))
 
-(defn get-co2-rating [input]
-  (loop [input-ratings input
-         position 0]
-    (if (= 1 (count input-ratings))
-      (first input-ratings)
-      (recur (filter #(matches-co2-criteria? input-ratings position %1) input-ratings) (inc position)))
-    ))
+(def get-o2-rating #(get-rating matches-o2-criteria? %))
+
+(def get-co2-rating #(get-rating matches-co2-criteria? %))
 
 (defn get-life-support-ratings [input-str]
   (let [input-ratings (parse-input input-str)]
